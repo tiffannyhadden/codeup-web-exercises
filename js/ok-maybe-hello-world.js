f5c2051432125e2f4b993a43d36a588c0ba00bb6
tiff <tiffanny.hadden@gmail.com>
2021-04-14 18:08:46 -0500

    adds Javascript with HTML

---
+++ js/ok-maybe-hello-world.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// MINI EXERCISE
+// TODO: I demand a haiku! Using alert, write a haiku!
+//     -> The entire poem consists of just three lines, with 17 syllables in total.
+//     -> The first line is 5 syllables.
+//     -> The second line is 7 syllables.
+//     -> The third line is 5 syllables.
+
+alert ("An old farmhouse bed");
+alert ("With soft white lavender sheets");
+alert ("is very comfy");
+
+
+
+
