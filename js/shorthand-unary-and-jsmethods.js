7b29457183f93129610b70dc64a0f72edb105c8a
tiff <tiffanny.hadden@gmail.com>
2021-04-15 16:15:11 -0500

    adds shorthand-unary-and-jsmethods

---
+++ js/shorthand-unary-and-jsmethods.js
@@ -0,0 +1,29 @@
+"use strict";
+// MINI EXERCISE
+
+// TODO: Refactor the mini exercise we did earlier on arithmetic operators to use variables and their related shorthand assignment operators, then log out the result
+//  ie:
+//     var result = 5;
+//     console.log(result += 1);
+
+
+// console.log(3 + 1);
+// console.log(5 - 1);
+// console.log(2 * 2);
+// console.log(8 / 2);
+// console.log(4 % 28);
+
+var result = 3;
+console.log(result += 1);
+
+var result = 5;
+console.log(result -= 1);
+
+var result = 2;
+console.log(result *= 2);
+
+var result = 8;
+console.log(result /= 2);
+
+var result = 4;
+console.log(result %= 28);
