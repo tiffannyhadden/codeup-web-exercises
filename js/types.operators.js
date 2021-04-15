e8e68970606719fe8ede2100d5892ced63d42862
tiff <tiffanny.hadden@gmail.com>
2021-04-15 10:13:49 -0500

    types.operators

---
+++ js/types.operators.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// var isTrue = true; // boolean value
+//
+// console.log("the value of isTrue: " + true); //boolean literal
+
+//----------------------------------
+
+// var iAmHungry = true;
+//
+// if (iAmHungry){
+//  console.log("Let's find some foooooood fam!");
+// }
+
+// var hoursSinceMeal = 5;
+
+// var iAmHungry = (hoursSinceMeal) > 4);
+
+//if (iAmHungry) { // boolean expression
+    //console.log("I'm getting a little hungry");
+//}
+
+// ---------------------------------NUMBERS
+
+//console.log("The number is: " + 10);
+//console.log("The number is: " + 19255435535735245);
+//console.log("The number is: " + -1754);
+//console.log("The number is: " + 14.2);
+
+//console.log(10 + 10); //addition operator
+
+//console.log(2 - 8); //subtraction operator
+
+//console.log(4 * 5); //multiplication operator
+
+//console.log(1 / 0); //division operator
+
+//console.log((-0) / 0);
+
+//console.log(12 % 3); // modulus operator
+
+// MINI EXERCISE WOOOOOOOOO
+
+// TODO: console.log() five questions using each arithmetic operator. Each equation should resolve to 4.
+
+
+console.log(3 + 1);
+console.log(5 - 1);
+console.log(2 * 2);
+console.log(8 / 2);
+console.log(4 % 28);
+
+
+
+
+
+
+
+
