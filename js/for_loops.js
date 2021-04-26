1979e41287cc27de35b9a978c54e02dc46ef0037
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-04-26 11:46:38 -0700

    adds iterating.js

---
+++ js/for_loops.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function showMultiplicationTable(number) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(number + " x " + i + " = " + i * number)
+    }
+}
+console.log(showMultiplicationTable(7));
+
+
