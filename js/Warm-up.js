8833ebca7b1d570622a9b97f86862842b49389cc
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-05-03 17:45:36 -0700

    adds 5/3 materials

---
+++ js/Warm-up.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Make a function named trim(string) that removes empty spaces before and after the input, the returns that trimmed string
+
+
+function trim(input) {
+    return input.replaceAll(' , ');
+}
+console.log(trim('Charles BBQed last night'));
+
