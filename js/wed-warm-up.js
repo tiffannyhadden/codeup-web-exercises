e39ccbff6db099066f15bd74d6108f61a23267f9
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-05-05 09:34:19 -0700

    adds wed-warm-up

---
+++ js/wed-warm-up.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+// Write a function called last(input) that returns the last character of a string
+
+function last(input) {
+    return input[input.length -1];
+}
+function last(input){
+    return input.slice(-1);
+}
