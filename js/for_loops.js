e5e0810d5930e42e0202ebd6be37208d62497488
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-05-10 19:44:59 -0700

    adds login-form.html

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,10 +1,12 @@
 "use strict";
 
-function showMultiplicationTable(number) {
-    for (var i = 1; i <= 10; i++) {
-        console.log(number + " x " + i + " = " + i * number)
-    }
-}
-console.log(showMultiplicationTable(7));
+// function showMultiplicationTable(number) {
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(number + " x " + i + " = " + i * number)
+//     }
+// }
+// console.log(showMultiplicationTable(7));
+
+
 
 
--- js/while.js
+++ js/while.js
@@ -2,35 +2,35 @@
 
 
 
-var i = 0;
-for(i=0; i<=25; i++){
-// let's divide the value by 2.
-// if the remainder is zero then it's an even number.
-    if(i % 2 == 0){
-        console. log(i);
-    }
-}
+// var i = 0;
+// for(i=0; i<=25; i++){
+// // let's divide the value by 2.
+// // if the remainder is zero then it's an even number.
+//     if(i % 2 == 0){
+//         console. log(i);
+//     }
+// }
+//
+//
+// var password;
+//
+// while (password !== "purple") {
+//     password = prompt("What is the password?");
+//
+//
+// }
 
 
-var password;
-
-while (password !== "purple") {
-    password = prompt("What is the password?");
-
-
-}
 
 
 
-
-
-var x = 2;
-
-while (x < 65537) {
-    console.log(x);
-    x *= 2;
-
-}
+// var x = 2;
+//
+// while (x < 65537) {
+//     console.log(x);
+//     x *= 2;
+//
+// }
 
 
 var allCones = Math.floor(Math.random() * 50) + 50;
