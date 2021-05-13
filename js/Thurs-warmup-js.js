7aa8258525fe9cd9a60403985b614511eb771b97
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-05-13 09:22:05 -0700

    adds thurs-warmup-js.js

---
+++ js/Thurs-warmup-js.js
@@ -0,0 +1,21 @@
+"use strict";
+// Write a JavaScript program to construct the following pattern, using a nested for loop.
+//                                                                                   *
+//                                                                                   * *
+//                                                                                   * * *
+//                                                                                   * * * *
+//                                                                                   * * * * *
+
+
+// Create a for loop that uses console.log to create the output shown below.
+
+
+//1
+//22
+//333
+//4444
+//55555
+
+for(let i = 1; i < 6; i++) {
+    console.log(i.toString().repeat(i));
+}
