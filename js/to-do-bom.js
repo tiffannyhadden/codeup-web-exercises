6673f6926ba721e69161131aeec284588f5b55b2
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-05-18 15:08:41 -0700

    adds to-do-bom.js

---
+++ js/to-do-bom.js
@@ -0,0 +1,20 @@
+//TODO: This program will run indefinitely. To finish coding this program, you’ll need to stop the setInterval() method after the showTime() function has run five times.
+//Save setInterval function to a variable
+//Pass the variable into the clearInterval function to stop the interval
+//Hint (It may be helpful to create an if statement
+let count = 0;
+function showTime() {
+    // return new date and time
+    let dateTime= new Date();
+    // return the time
+    let time = dateTime.toLocaleTimeString();
+    count += 1;
+    //if the count reaches 5, stop the program.
+
+    if(count === 5){
+        clearInterval(display);
+    }
+    console.log(time)
+}
+let display = setInterval(showTime, 5000);
+
