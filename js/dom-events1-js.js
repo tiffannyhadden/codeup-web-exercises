a67ef56175c6dfb1a6606a04223f78beaf4bd64c
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-05-21 16:01:37 -0700

    adds changes to dom-events1-html and dom-events1-js

--- js/dom-events1-js.js
+++ js/dom-events1-js.js
@@ -1,7 +1,32 @@
-
 function addButtonEvent() {
     document.getElementById("submitBtn")
         .addEventListener("click", addPost)
 }
 function addPost() {
+
+    var textArea = document.getElementById("user-input")
+    var newPost = document.createElement("textarea")
+    newPost.innerText = textArea.value
+    document.getElementById("blog-container").append(newPost);
+    addEmail();
+}
+
+addButtonEvent()
+
+function addEmail(){
+
+    // create a element
+
+    let emailAdd = document.getElementById("email");
+
+    // create another element and have it set to the new text area
+
+    let postE = document.createElement('textarea');
+
+    // append the email input into the new textarea
+
+    postE.textContent = emailAdd.value;
+
+    document.getElementById("blog-container").append(postE);
+
 }
