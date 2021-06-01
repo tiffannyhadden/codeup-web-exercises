6044850675c4bfb48961dea28be00df3fec732a7
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-01 14:09:21 -0700

    adds jquery

---
+++ js/jquery.js
@@ -0,0 +1,7 @@
+// TODO: Using jQuery ... 1. hide the paragraph that is in all caps  2.hide all the spans 3. show all the elements that have a ‘show-me’ class. refresh your page between every task.
+//  d
+$("#all-caps").hide();
+
+$("span").hide();
+
+$(".show-me").show();
