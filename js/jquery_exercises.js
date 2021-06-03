a726fd8ef4ea7646f4995cce300595580da56105
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-03 14:12:00 -0700

    adds finishes

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -3,13 +3,14 @@ $(document).ready(function() {
 });
 
 $("#header").css("color", "darkblue")
-alert($("header")[0].innerText);
+alert($("#header")[0].innerText);
 $(".first").css("font-size", "30px")
 $("#list").css("color", "darkgreen")
 $(".codeup").css("border", "1px solid red")
 $("li").css("font-size", "20px")
 $(".craft").css( {
-    "background-color": "fff2ac",
+    "background-color": "#fff2ac",
     "background-image": "linear-gradient(to right, #ffe359 0%, #fff2ac 100%)"
 })
 alert($("h1")[0].innerText)
+$(".funk").css("font-variant-numeric", "ordinal")
