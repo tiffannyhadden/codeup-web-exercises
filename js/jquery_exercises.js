ae7e510977f331249a5059d4f21c9453f51443e8
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-02 17:45:54 -0700

    adds changes

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
+
+$("#header").css("color", "darkblue")
+alert($("header")[0].innerText);
+$(".first").css("font-size", "30px")
+$("#list").css("color", "darkgreen")
+$(".codeup").css("border", "1px solid red")
+$("li").css("font-size", "20px")
+$(".craft").css( {
+    "background-color": "fff2ac",
+    "background-image": "linear-gradient(to right, #ffe359 0%, #fff2ac 100%)"
+})
+alert($("h1")[0].innerText)
