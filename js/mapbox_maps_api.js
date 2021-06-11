594057f3ff426f27133f7026f477d0dcce5cd763
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-10 22:21:53 -0700

    adds keys, weather_map

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -7,13 +7,39 @@ var map = new mapboxgl.Map({
     zoom: 9
 });
 
-var whiskeyCakeMarker = new mapboxgl.Marker()
-    .setLngLat([-98.61821, 29.59339])//whiskey cake
-    .addTo(map);
+// var whiskeyCakeMarker = new mapboxgl.Marker()
+//     .setLngLat([-98.61821, 29.59339])//whiskey cake
+//     .addTo(map);
 
-var whiskeyPopup = new mapboxgl.Popup()
-    .setHTML("<p> mmm Whiskey Cake!</p>") ///mmm whiskey  cake
-    .addTo(map)
+// var whiskeyPopup = new mapboxgl.Popup()
+//     .setHTML("<p> mmm Whiskey Cake!</p>") ///mmm whiskey  cake
+//     .addTo(map)
+//
+// whiskeyCakeMarker.setPopup(whiskeyPopup)
 
-whiskeyCakeMarker.setPopup(whiskeyPopup)
 
+function markerMaker(rest) {
+    var popup = new mapboxgl.Popup()
+        .setHTML("<h1>" + rest.name + "</h1>")
+        .setMaxWidth("200px");
+    var marker = new mapboxgl.Marker()
+        .setLngLat(rest.location)
+        .setDraggable(true)
+        .setPopup(popup)
+        .addTo(map);
+}
+var myFavs = [
+    {
+        name: 'Whiskey Cake',
+        location: [-98.61821, 29.59339]
+    },
+    {
+        name: 'Pappadeaux',
+        location: [-98.48815, 29.51985]
+    },
+    {
+        name: 'La Fogata',
+        location: [-98.53531, 29.49592]
+    },
+];
+myFavs.forEach(markerMaker);
---
+++ js/weather_map.js
@@ -0,0 +1,31 @@
+
+initWeather();
+initMap();
+function initWeather() {
+    $.get("https://api.openweathermap.org/data/2.5/forecast", {
+        APPID: OPEN_WEATHER_APPID,
+        lat:    29.423017,
+        lon:   -98.48527,
+        units: "imperial"
+    }).done(function (data) {
+        console.log(data)
+        function buildList(data) {
+
+            let output = "";
+            output += "<p>" + "<strong> Date: </strong>" + "</p>" + data.list[0].dt_txt;
+            output += "<p>" + "<strong> Temp: </strong>" +  Math.round(data.list[0].main.temp_max) + "\xB0" + " / " + Math.round(data.list[0].main.temp_min) +"\xB0"+ "</p>";
+            output += "<p>" + "<strong> Humidity: </strong>" + data.list[0].main.humidity;
+            return output;
+        }
+
+        $('#weather').html(buildList(data));
+    });
+}
+
+function initMap() {
+    //this is where i'm going to put map
+    //MAPBOX_TOKEN here is my map id token const
+
+}
+
+
